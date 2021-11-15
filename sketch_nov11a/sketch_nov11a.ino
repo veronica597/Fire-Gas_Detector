9dc94c89498c16787827eab2843d2ee9dc7d3b09
veronicapecchioli <veronica.pecchioli@outlook.it>
2021-11-16 00:52:29 +0100

    aggiunto codice sensore MQ-2

--- sketch_nov11a/sketch_nov11a.ino
+++ sketch_nov11a/sketch_nov11a.ino
@@ -1,34 +1,60 @@
-
+int sogliaGas = 400;
+int sogliaFiamma =350;
 
 void setup() {
+  
 pinMode (8, OUTPUT); // direzione del pin buzzer
-//pinMode (12, OUTPUT); // direzione del pin led
+//pinMode (12, OUTPUT); // direzione del pin led rosso
+//pinMode (11, OUTPUT);  // direzione del pin led verde
+//pinMode ( A5, INPUT); // leggo dati in ingresso dal sensore di gas MQ-2
 Serial.begin (9600); // avvio del monitor seriale
 }
  
 void loop() {
 int fiamma = analogRead(A0); // rilevo il valore da A0
+int gas = analogRead (A5); // rilevo il valore da A5
+
 Serial.println(fiamma); // stampo su monitor seriale
+
+Serial.print("valore del MQ-2: ");
+Serial.println(gas);
+
+if (gas > sogliaGas){
+    cambioLed();
+    sound();
+  }
  
-if (fiamma > 350) { // se il valore supera 350...
+if (fiamma > sogliaFiamma){
  
-alarm(); // faccio lampeggiare il led e faccio suonare il buzzer
-sound();
+    cambioLed(); 
+    sound();
+    
+  }
+else { 
+    LedVerde();
+    noTone(8); // non fa suonare il buzzer
+  }
 }
-else {
+
+
+
+
+void LedVerde(){ // spengo led rosso e accendo led verde
+  digitalWrite(12,LOW);
+  digitalWrite(11,HIGH);
+  }
  
-// altrimenti non fa nulla
-}
-}
- 
-void alarm(){
-digitalWrite (12, HIGH);
+void cambioLed(){ // funzione per scambiare i led da verde a rosso, e lo fa lampeggiare i led
+digitalWrite (11, LOW); // spengo led verde
+digitalWrite (12, HIGH); // accendo led rosso
+
 delay (500);
-digitalWrite (12, LOW);
+
+digitalWrite (12, LOW); // spengo led rosso
 delay (500);
 }
  
-void sound(){
+void sound(){ // funzione per far suonare il buzzer
 digitalWrite (8, HIGH);
 delay (1000);
 digitalWrite (8, LOW);
