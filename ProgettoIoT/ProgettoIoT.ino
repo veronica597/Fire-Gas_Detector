22a41ab14ca62cbc1a97fdda417910837db3e13d
veronicapecchioli <veronica.pecchioli@outlook.it>
2021-11-24 22:10:08 +0100

    aggiunte configurazioni per collegare all'hotspot telefono

--- ProgettoIoT/ProgettoIoT.ino
+++ ProgettoIoT/ProgettoIoT.ino
@@ -8,7 +8,7 @@ char pass[] = SECRET_PASS;    // your network password
 WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient); // client MQTT
 
-const char broker[] = "192.168.1.5";
+const char broker[] = "";//192.168.1.5
 int        port     = 1883;
 const char topic[]  = "fire_warning";
 //const char topic2[]  = "real_unique_topic_2";
@@ -39,9 +39,6 @@ int digitalPinMQ2 = 4; //MQ-2 digital interface
 int digitalValMQ2;
 
 
-//variabili per SHT31D:
-//bool enableHeater = false;
-//uint8_t loopCnt = 0;
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
 
 //variabili per lettura digitale KY-026:
@@ -54,7 +51,6 @@ int digitalVal=0;
 void setup() {
   Serial.begin (9600); // avvio del monitor seriale
   
-
 
   /*tentativo di connessione alla rete wifi e al broker mqtt*/ 
   Serial.print("Attempting to connect to WPA SSID: ");
@@ -109,8 +105,7 @@ void setup() {
 
 
 void loop() {
-  //digitalWrite(11, HIGH); //parto con led verde acceso
-
+  
   mqttClient.poll(); // manda mess di keep alive per evitare di essere disconnessi dal broker (Raspberry)
   
 //  mqttClient.subscribe(fire_warning);
@@ -141,8 +136,7 @@ void loop() {
 
     Serial.print("Sending message to topic: ");
     Serial.println(topic);
-    Serial.println("hello");
-
+    
     mqttClient.beginMessage(topic);
     mqttClient.println(Fire_Warning);
     mqttClient.println(smoke);
@@ -187,64 +181,3 @@ void sound() {
   digitalWrite (8, LOW);
   delay (1000);
 }
-
-
-
-
-
-
-
-
-
-//funzioni non usate nel loop, le conservo per sicurezza:
-
-/*funzione per fare il setup necessario per il sensore SHT-31D*/
-//void setupSHT31D() {
-//  Serial.println("SHT31 test");
-//  if (!sht31.begin(0x44)) {   // Set to 0x45 for alternate i2c addr
-//    Serial.println("Couldn't find SHT31");
-//    while (1) delay(1);
-//  }
-//
-//  /*Serial.print("Heater Enabled State: ");
-//    if (sht31.isHeaterEnabled())
-//    Serial.println("ENABLED");
-//    else
-//    Serial.println("DISABLED");
-//    }*/
-//
-//}
-
-/*funzione per calcolo temperatura/umidità*/
-//float SHT31D() {
-//  float t = sht31.readTemperature();
-//  float h = sht31.readHumidity();
-//
-//  if (! isnan(t)) {  // check if 'is not a number'
-//    Serial.print("Temp *C = "); Serial.print(t); Serial.print("\t\t");
-//  } else {
-//    Serial.println("Failed to read temperature");
-//  }
-//
-//  if (! isnan(h)) {  // check if 'is not a number'
-//    Serial.print("Hum. % = "); Serial.println(h);
-//  } else {
-//    Serial.println("Failed to read humidity");
-//  }
-//
-//  delay(1000);
-//
-//  // Toggle heater enabled state every 30 seconds
-//  // An ~3.0 degC temperature increase can be noted when heater is enabled
-//  if (++loopCnt == 30) {
-//    enableHeater = !enableHeater;
-//    sht31.heater(enableHeater);
-//    Serial.print("Heater Enabled State: ");
-//    if (sht31.isHeaterEnabled())
-//      Serial.println("ENABLED");
-//    else
-//      Serial.println("DISABLED");
-//
-//    loopCnt = 0;
-//  }
-//}
